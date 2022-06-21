f3bee0701087de21a245a8d0df786c442aa8c1c7
htm2323 <83809067+htm2323@users.noreply.github.com>
2022-06-21 15:40:40 +0900

    MapAPIテスト

--- app/src/main/java/jp/ac/ritsumei/ise/phy/exe3/is0578ri/ritsumeimap/MapsActivity.java
+++ app/src/main/java/jp/ac/ritsumei/ise/phy/exe3/is0578ri/ritsumeimap/MapsActivity.java
@@ -8,18 +8,22 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Locale;
+
 import jp.ac.ritsumei.ise.phy.exe3.is0578ri.ritsumeimap.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
+{
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
@@ -41,12 +45,34 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap)
+    {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng bkc = new LatLng(34.981984561994466, 135.9623099219572);
+        mMap.addMarker(new MarkerOptions().position(bkc).title("Marker in BKC").snippet("補足"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bkc, 16));
+
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng tapLocation) {
+                LatLng location;
+                location = new LatLng(tapLocation.latitude, tapLocation.longitude);
+                String str = String.format(Locale.US, "%f, %f", tapLocation.latitude, tapLocation.longitude);
+                mMap.addMarker(new MarkerOptions().position(location).title(str).snippet("TapTest"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
+            }
+        });
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener(){
+            @Override
+            public void onMapLongClick(LatLng longTapLocation){
+                LatLng newLocation = new LatLng(longTapLocation.latitude, longTapLocation.longitude);
+                mMap.addMarker(new MarkerOptions().position(newLocation)
+                        .title("" + longTapLocation.latitude + " :" + longTapLocation.longitude)
+                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_fukidasi)));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newLocation, 18));
+            }
+        });
     }
 }
