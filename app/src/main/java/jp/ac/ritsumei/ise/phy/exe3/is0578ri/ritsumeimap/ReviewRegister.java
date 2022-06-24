c64f69f477489e35ee52b0cdc8240ee08a6dbb61
htm2323 <83809067+htm2323@users.noreply.github.com>
2022-06-24 10:14:38 +0900

    ReviewRegisterアクティビティ作成

---
+++ app/src/main/java/jp/ac/ritsumei/ise/phy/exe3/is0578ri/ritsumeimap/ReviewRegister.java
@@ -0,0 +1,14 @@
+package jp.ac.ritsumei.ise.phy.exe3.is0578ri.ritsumeimap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReviewRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_register);
+    }
+}
