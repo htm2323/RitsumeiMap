0259b8ba86f9907b07f35cc61a812e48aa133d49
htm2323 <83809067+htm2323@users.noreply.github.com>
2022-06-29 10:17:44 +0900

    NCMBの初期化を挿入

--- app/src/main/java/jp/ac/ritsumei/ise/phy/exe3/is0578ri/ritsumeimap/SplashScreen.java
+++ app/src/main/java/jp/ac/ritsumei/ise/phy/exe3/is0578ri/ritsumeimap/SplashScreen.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 
+import com.nifcloud.mbaas.core.NCMB;
+
 public class SplashScreen extends AppCompatActivity
 {
     @Override
@@ -14,6 +16,10 @@ public class SplashScreen extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        NCMB.initialize(this.getApplicationContext(),
+                "242065d2599016f30f6c72b88582430b94e97efe9b9aacdfe55875e16f740f9f",
+                "da8cbfbb576f6c3ef0205be62528c7effe0fb5b9b7464d4604ccff0b004a67aa");
 
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable()
         {
